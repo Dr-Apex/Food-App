4ac620d07974379c9f3ab16d73889a95bda8276b
Aayush <mahapatraaayush@gmail.com>
2020-09-14 12:48:22 +0530

    Update zomato.js

--- src/api/zomato.js
+++ src/api/zomato.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://developers.zomato.com/api/v2.1',
   headers: {
-    'user-key': '99d955301594bc1b81ba69fef3d76d21'
+    'user-key': ''
   }
 });
